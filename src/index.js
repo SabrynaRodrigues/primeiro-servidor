c79a1e908944c1b71d56a008577b36f28462ffe6
Sabryna <sabrynarodrigues1804@gmail.com>
2022-11-12 13:33:54 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const router = express.Router()
+
+router.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/home.html"))
+})
+
+router.get("/contato", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+app.use(router)
+
+app.listen(3333, ()=>{
+    console.log("server is working")
+})
+
+
